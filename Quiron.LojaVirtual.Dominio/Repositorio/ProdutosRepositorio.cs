9f07515610000521fd9077e68e18f5453ad2a556
alanrussodev <alanrusso@live.com>
2014-07-31 12:59:21 -0300

    Entity Framework Beginning

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quiron.LojaVirtual.Dominio.Entidade;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+
+        private  readonly EfDbContext _context = new EfDbContext();
+
+        public IEnumerable<Produto> Produtos
+
+        {
+            get { return _context; }
+        }
+
+    }
+}
